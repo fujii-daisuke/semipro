0aa6c521521de376a8cec82062ab1a5c08e4c0ed
fujiidaisuke <fujiidaisuke@192.168.0.12>
2019-04-02 22:56:58 +0900

    pagination add

---
+++ semipro-domain/src/main/java/red/semipro/domain/repository/seminar/SeminarSearchCriteria.java
@@ -0,0 +1,12 @@
+package red.semipro.domain.repository.seminar;
+
+import java.io.Serializable;
+
+public class SeminarSearchCriteria implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+}
