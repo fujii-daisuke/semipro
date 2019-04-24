64edcfded3b609659fef2b37ca4080ce80ed116d
d-fujii <fujii.daisuke@goingflow.com>
2019-04-25 07:56:15 +0900

    activationed mail add

--- semipro-web/src/main/java/red/semipro/app/activation/ActivationController.java
+++ semipro-web/src/main/java/red/semipro/app/activation/ActivationController.java
@@ -1,12 +1,19 @@
 package red.semipro.app.activation;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import red.semipro.business.email.EmailDocument;
+import red.semipro.business.email.EmailService;
 import red.semipro.common.Crypto;
 import red.semipro.domain.enums.RegisterStatus;
 import red.semipro.domain.model.Member;
@@ -17,11 +24,15 @@ import red.semipro.domain.service.member.MemberService;
 public class ActivationController {
 
     @Autowired
-    MemberService memberService;
+    private MemberService memberService;
+    @Autowired
+    private EmailService emailService;
+    @Value("${custom.application.email.fromEmail}")
+    private String fromEmail;
     
     @GetMapping(value="{activationKey}/register")
     public ModelAndView register(@PathVariable("activationKey") String activationKey,
-            ModelAndView model) throws Exception {
+            ModelAndView model, Locale locale) throws Exception {
         
         Crypto crypto = new Crypto();
         String memberId = crypto.decrypto(activationKey);
@@ -34,6 +45,7 @@ public class ActivationController {
             return model;
         }
         memberService.activation(member);
+        sendMail(member, locale);
         model.setViewName("redirect:/activation/completed");
         return model;
     }
@@ -50,4 +62,14 @@ public class ActivationController {
         return model;
     }
 
+    private void sendMail(Member member, Locale locale) throws Exception {
+        Map<String, Object> variableMap = new HashMap<String, Object>();
+        variableMap.put("member", member);
+        emailService.sendMail(
+                EmailDocument.ACTIVATED,
+                variableMap, 
+                member.getEmail(), 
+                fromEmail, 
+                locale);
+    }
 }
