ec806bb375df1a60bf807c5d561e643591eedf7a
d-fujii <fujii.daisuke@goingflow.com>
2019-04-25 20:17:55 +0900

    spring security authentication add

--- semipro-web/src/main/java/red/semipro/app/login/LoginController.java
+++ semipro-web/src/main/java/red/semipro/app/login/LoginController.java
@@ -1,11 +1,8 @@
 package red.semipro.app.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -24,20 +21,7 @@ public class LoginController {
         return modelAndView;
     }
     
-    @PostMapping("signin")
-    public ModelAndView signin(ModelAndView modelAndView,
-            @Validated LoginForm loginForm,
-            BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            modelAndView.setViewName("login/login");
-            return modelAndView;
-        }
-        
-        modelAndView.setViewName("forward:/authenticate");
-        return modelAndView;
-    }
-    
-    @GetMapping("failure")
+    @RequestMapping("failure")
     public ModelAndView failure(ModelAndView modelAndView,
             @ModelAttribute("loginForm") LoginForm loginForm) {
         modelAndView.setViewName("login/login");
