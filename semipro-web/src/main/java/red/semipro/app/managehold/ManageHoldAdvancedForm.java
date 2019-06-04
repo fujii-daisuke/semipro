987f266ad0390a3448bfd4223e094b32a7ddcb18
d-fujii <fujii.daisuke@goingflow.com>
2019-06-04 14:08:09 +0900

    seminar advanced form update

--- semipro-web/src/main/java/red/semipro/app/managehold/ManageHoldAdvancedForm.java
+++ semipro-web/src/main/java/red/semipro/app/managehold/ManageHoldAdvancedForm.java
@@ -3,10 +3,14 @@ package red.semipro.app.managehold;
 import java.io.Serializable;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Data;
+import red.semipro.common.upload.UploadFileMaxSize;
+import red.semipro.common.upload.UploadFileNotEmpty;
+import red.semipro.common.upload.UploadFileRequired;
 @Data
 public class ManageHoldAdvancedForm implements Serializable {
 
@@ -14,11 +18,16 @@ public class ManageHoldAdvancedForm implements Serializable {
      * 
      */
     private static final long serialVersionUID = 1L;
-    @NotNull
+    
+    @UploadFileRequired
+    @UploadFileNotEmpty
+    @UploadFileMaxSize
     private MultipartFile mainImage;
     @NotNull
+    @Size(max=200)
     private String summary;
     @NotNull
+    @Size(max=10000)
     private String contents;
     @NotNull
     private Boolean shootingSupported;
