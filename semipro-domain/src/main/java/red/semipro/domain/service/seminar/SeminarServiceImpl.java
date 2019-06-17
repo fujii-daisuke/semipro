7b6ff23de296dd329490e33b9abe42fcf4aa10aa
d-fujii <fujii.daisuke@goingflow.com>
2019-06-18 08:38:39 +0900

    ticket form update

--- semipro-domain/src/main/java/red/semipro/domain/service/seminar/SeminarServiceImpl.java
+++ semipro-domain/src/main/java/red/semipro/domain/service/seminar/SeminarServiceImpl.java
@@ -51,9 +51,9 @@ public class SeminarServiceImpl implements SeminarService {
     public Seminar findOneWithDetails(Long id) {
         Seminar seminar = seminarRepository.findOneWithDetails(id);
         if (seminar != null) {
-            seminar.setSeminarTickets(seminarTicketRepository.findAll(seminar.getId()));
-            seminar.setSeminarPaymentTypes(seminarPaymentTypeRepository.findAll(seminar.getId()));
-            seminar.setSeminarOwners(seminarOwnerRepository.findAll(seminar.getId()));
+            seminar.setTickets(seminarTicketRepository.findAll(seminar.getId()));
+            seminar.setPaymentTypes(seminarPaymentTypeRepository.findAll(seminar.getId()));
+            seminar.setOwners(seminarOwnerRepository.findAll(seminar.getId()));
         }
         return seminar;
     }
@@ -62,17 +62,17 @@ public class SeminarServiceImpl implements SeminarService {
         Seminar oldSeminar = seminarRepository.findOneByProviderIdAndProviderSeminarId(seminar.getProviderId().getValue(), seminar.getProviderSeminarId());
         if (oldSeminar == null) {
             seminarRepository.insert(seminar);
-            if (seminar.getSeminarPaymentTypes() != null && seminar.getSeminarPaymentTypes().size() > 0) {
-                seminar.getSeminarPaymentTypes().forEach(pay -> pay.setSeminarId(seminar.getId()));
-                seminarPaymentTypeRepository.insert(seminar.getSeminarPaymentTypes());
+            if (seminar.getPaymentTypes() != null && seminar.getPaymentTypes().size() > 0) {
+                seminar.getPaymentTypes().forEach(pay -> pay.setSeminarId(seminar.getId()));
+                seminarPaymentTypeRepository.insert(seminar.getPaymentTypes());
             }
-            if (seminar.getSeminarTickets() != null && seminar.getSeminarTickets().size() > 0) {
-                seminar.getSeminarTickets().forEach(ticket -> ticket.setSeminarId(seminar.getId()));
-                seminarTicketRepository.insert(seminar.getSeminarTickets());
+            if (seminar.getTickets() != null && seminar.getTickets().size() > 0) {
+                seminar.getTickets().forEach(ticket -> ticket.setSeminarId(seminar.getId()));
+                seminarTicketRepository.insert(seminar.getTickets());
             }
-            if (seminar.getSeminarOwners() != null && seminar.getSeminarOwners().size() > 0) {
-                seminar.getSeminarOwners().forEach(owner -> owner.setSeminarId(seminar.getId()));
-                seminarOwnerRepository.insert(seminar.getSeminarOwners());
+            if (seminar.getOwners() != null && seminar.getOwners().size() > 0) {
+                seminar.getOwners().forEach(owner -> owner.setSeminarId(seminar.getId()));
+                seminarOwnerRepository.insert(seminar.getOwners());
             }
             return;
         }
@@ -86,17 +86,17 @@ public class SeminarServiceImpl implements SeminarService {
         seminarPaymentTypeRepository.delete(seminar.getId());
         seminarTicketRepository.delete(seminar.getId());
         seminarOwnerRepository.delete(seminar.getId());
-        if (seminar.getSeminarPaymentTypes() != null && seminar.getSeminarPaymentTypes().size() > 0) {
-            seminar.getSeminarPaymentTypes().forEach(pay -> pay.setSeminarId(seminar.getId()));
-            seminarPaymentTypeRepository.insert(seminar.getSeminarPaymentTypes());
+        if (seminar.getPaymentTypes() != null && seminar.getPaymentTypes().size() > 0) {
+            seminar.getPaymentTypes().forEach(pay -> pay.setSeminarId(seminar.getId()));
+            seminarPaymentTypeRepository.insert(seminar.getPaymentTypes());
         }
-        if (seminar.getSeminarTickets() != null && seminar.getSeminarTickets().size() > 0) {
-            seminar.getSeminarTickets().forEach(ticket -> ticket.setSeminarId(seminar.getId()));
-            seminarTicketRepository.insert(seminar.getSeminarTickets());
+        if (seminar.getTickets() != null && seminar.getTickets().size() > 0) {
+            seminar.getTickets().forEach(ticket -> ticket.setSeminarId(seminar.getId()));
+            seminarTicketRepository.insert(seminar.getTickets());
         }
-        if (seminar.getSeminarOwners() != null && seminar.getSeminarOwners().size() > 0) {
-            seminar.getSeminarOwners().forEach(owner -> owner.setSeminarId(seminar.getId()));
-            seminarOwnerRepository.insert(seminar.getSeminarOwners());
+        if (seminar.getOwners() != null && seminar.getOwners().size() > 0) {
+            seminar.getOwners().forEach(owner -> owner.setSeminarId(seminar.getId()));
+            seminarOwnerRepository.insert(seminar.getOwners());
         }
     }
 
