bdbd63821dc1750e91a9e24fde735934e5390780
d-fujii <fujii.daisuke@goingflow.com>
2020-04-27 19:36:25 +0900

    未コミット

---
+++ semipro-web/src/main/java/red/semipro/app/signup/SignUpFormConverter.java
@@ -0,0 +1,34 @@
+package red.semipro.app.signup;
+
+import javax.annotation.Nonnull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import red.semipro.domain.enums.RegisterStatus;
+import red.semipro.domain.model.account.Account;
+
+/**
+ * 会員登録フォーム - converter
+ */
+@Component
+@RequiredArgsConstructor
+public class SignUpFormConverter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * 会員登録フォームをアカウントにコンバートする
+     *
+     * @param signUpForm 会員登録フォーム
+     * @return アカウント
+     */
+    Account convert(@Nonnull final SignUpForm signUpForm) {
+        return Account.builder()
+            .email(signUpForm.getEmail())
+            .username(signUpForm.getUsername())
+            .password(passwordEncoder.encode(signUpForm.getPassword()))
+            .registerStatus(RegisterStatus.PROVISIONAL)
+            .build();
+    }
+
+}
