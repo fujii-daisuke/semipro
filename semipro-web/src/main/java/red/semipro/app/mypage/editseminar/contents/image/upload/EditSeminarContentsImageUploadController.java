5b170eb356f81ab36873def79bb25c502d9dc7af
d-fujii <fujii.daisuke@goingflow.com>
2020-05-06 14:44:48 +0900

    メッセージ修正

--- semipro-web/src/main/java/red/semipro/app/mypage/editseminar/contents/image/upload/EditSeminarContentsImageUploadController.java
+++ semipro-web/src/main/java/red/semipro/app/mypage/editseminar/contents/image/upload/EditSeminarContentsImageUploadController.java
@@ -36,7 +36,7 @@ public class EditSeminarContentsImageUploadController {
         @ModelAttribute @Validated EditSeminarContentsImageUploadForm form, BindingResult result) {
 
         if (result.hasErrors()) {
-            return ResponseEntity.badRequest().body("ファイルアップロードに失敗しました。画面を再度開き直してください。");
+            return ResponseEntity.badRequest().body("ファイルサイズが上限をオーバーしています。");
         }
 
         final String fileUrl;
