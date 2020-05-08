7a260643d4167bb2f9c2d0acc8062b29b84f68aa
d-fujii <fujii.daisuke@goingflow.com>
2020-05-08 12:10:51 +0900

    起動設定調整

---
+++ .codedeploy/scripts/application_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+systemctl start semipro-web.service
+systemctl start semipro-admin.service
+systemctl start semipro-batch.service
---
+++ .codedeploy/scripts/application_stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+systemctl stop semipro-web.servicee
+systemctl stop semipro-admin.service
+systemctl stop semipro-batch.service
