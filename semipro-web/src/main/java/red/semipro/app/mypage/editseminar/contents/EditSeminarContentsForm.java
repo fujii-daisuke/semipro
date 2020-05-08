dc1058fc6c1d9db3f2cb19d893df25c4c61623ab
d-fujii <fujii.daisuke@goingflow.com>
2020-05-08 14:39:19 +0900

    不具合修正

--- semipro-web/src/main/java/red/semipro/app/mypage/editseminar/contents/EditSeminarContentsForm.java
+++ semipro-web/src/main/java/red/semipro/app/mypage/editseminar/contents/EditSeminarContentsForm.java
@@ -3,14 +3,18 @@ package red.semipro.app.mypage.editseminar.contents;
 import java.io.Serializable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * セミナーコンテンツ - form
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EditSeminarContentsForm implements Serializable {
 
     private static final long serialVersionUID = -3006399735074074589L;
