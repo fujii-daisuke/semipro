c535bcede5d89005c67edddcbb126977224ffac8
d-fujii <fujii.daisuke@goingflow.com>
2020-05-08 13:06:07 +0900

    起動設定調整

--- .codedeploy/scripts/before_install.sh
+++ .codedeploy/scripts/before_install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-systemctl stop httpd.service
-systemctl stop semipro-web.servicee
-systemctl stop semipro-admin.service
-systemctl stop semipro-batch.service
-
 export FOLDER=/tmp/semipro-release/
 
 if [ -d $FOLDER ]
