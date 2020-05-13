c6ef5e692a3c6872aa3d313771cf05fea5313b22
d-fujii <fujii.daisuke@goingflow.com>
2020-05-13 11:08:14 +0900

    セミナー詳細画面が表示されない件の修正

--- semipro-web/src/main/java/red/semipro/app/entryseminar/EntrySeminarForm.java
+++ semipro-web/src/main/java/red/semipro/app/entryseminar/EntrySeminarForm.java
@@ -2,18 +2,22 @@ package red.semipro.app.entryseminar;
 
 import java.io.Serializable;
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * セミナーチケット - form
  */
 @Data
 @Builder
-public class EntrySeminarForm implements Serializable {
+@NoArgsConstructor
+@AllArgsConstructor
+    public class EntrySeminarForm implements Serializable {
 
-    private static final long serialVersionUID = -3483045843199265872L;
+        private static final long serialVersionUID = -3483045843199265872L;
 
-    @NotNull
+        @NotNull
     private String selectedStripeCustomerCardId;
 }
