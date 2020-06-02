25bfcedbf1de3fa8d0c8e7953c8757c375022fc2
d-fujii <fujii.daisuke@goingflow.com>
2020-06-02 11:55:09 +0900

    リファクタリング

--- semipro-batch/src/main/java/red/semipro/batch/endrecruitmentseminar/EndRecruitmentSeminarTask.java
+++ semipro-batch/src/main/java/red/semipro/batch/endrecruitmentseminar/EndRecruitmentSeminarTask.java
@@ -5,20 +5,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import red.semipro.domain.service.establish.EstablishService;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class EndRecruitmentSeminarTask {
 
-    private final EndRecruitmentSeminarHelper endRecruitmentSeminarHelper;
+    private final EstablishService establishService;
 
     @Scheduled(cron = "${custom.application.cron.endrecruitmentseminar}")
     public void execute() {
 
         log.info("セミナー募集終了処理を開始します");
 
-        endRecruitmentSeminarHelper.execute(LocalDate.now());
+        establishService.execute(LocalDate.now());
 
         log.info("セミナー募集終了処理を終了します");
 
